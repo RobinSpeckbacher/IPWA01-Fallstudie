4c58ddd417fbec1bf0f38aa36aa9d617426d0836
Robin Speckbacher <105314743+AwkwardturtleRobin@users.noreply.github.com>
2024-01-28 16:08:23 +0100

    Kleiderspender Setup

---
+++ kleiderspende/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header/Header"
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
---
+++ kleiderspende/src/pages/Spenden.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header"
+import React from 'react'
+
+const Spenden = () => {
+  return (
+    <>
+        <Header />
+        <h2>HomePage</h2>
+    </>
+  )
+}
+
+export default Spenden
